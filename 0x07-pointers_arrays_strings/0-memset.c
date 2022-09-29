4958e889f9849f2434b670905ef99185a7feffa8
hikomecha <hikomecha987@gmail.com>
2022-09-29 09:27:52 -0700

    my first commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with const.
+ * @s: source string
+ *
+ * @b: the constant byte for filling
+ *
+ * @n: length of buffer
+ * Return: new string
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	        unsigned int i = 0;
+
+		        while (i < n)
+			        {
+		                *(s + i) = b;
+				i++;
+				}
+	return (s);
+}
